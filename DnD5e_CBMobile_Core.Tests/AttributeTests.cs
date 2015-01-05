ae1264137cd58c295d034db6ccfddd53e1f862f3
Neil Sorensen <nsorens@gmail.com>
2015-01-04 23:38:48 -0700

    attributes can be added and generate correct modifiers

--- DnD5e_CBMobile_Core.Tests/AttributeTests.cs
+++ DnD5e_CBMobile_Core.Tests/AttributeTests.cs
@@ -14,6 +14,67 @@ namespace DnD5e_CBMobile_Core.Tests
 			var modified = baseAttr + modifier;
 			Assert.That (modified.Strength, Is.EqualTo (12));
 		}
+
+		[Test]
+		public void AddingUpdatesDex()
+		{
+			Attributes baseAttr = new Attributes{ Dexterity = 10 };
+			Attributes modifier = new Attributes{ Dexterity = 2 };
+			var modified = baseAttr + modifier;
+			Assert.That (modified.Dexterity, Is.EqualTo (12));
+		}
+
+		[Test]
+		public void AddingUpdatesConstitution()
+		{
+			Attributes baseAttr = new Attributes{ Constitution = 10 };
+			Attributes modifier = new Attributes{ Constitution = 2 };
+			var modified = baseAttr + modifier;
+			Assert.That (modified.Constitution, Is.EqualTo (12));
+		}
+
+		[Test]
+		public void AddingUpdatesInt()
+		{
+			Attributes baseAttr = new Attributes{ Intelligence = 10 };
+			Attributes modifier = new Attributes{ Intelligence = 2 };
+			var modified = baseAttr + modifier;
+			Assert.That (modified.Intelligence, Is.EqualTo (12));
+		}
+
+		[Test]
+		public void AddingUpdatesWisdom()
+		{
+			Attributes baseAttr = new Attributes{ Wisdom = 10 };
+			Attributes modifier = new Attributes{ Wisdom = 2 };
+			var modified = baseAttr + modifier;
+			Assert.That (modified.Wisdom, Is.EqualTo (12));
+		}
+
+		[Test]
+		public void AddingUpdatesCharisma()
+		{
+			Attributes baseAttr = new Attributes{ Charisma = 10 };
+			Attributes modifier = new Attributes{ Charisma = 2 };
+			var modified = baseAttr + modifier;
+			Assert.That (modified.Charisma, Is.EqualTo (12));
+		}
+
+		[TestCase(7,-2)]
+		[TestCase(8, -1)]
+		[TestCase(9, -1)]
+		[TestCase(10, 0)]
+		[TestCase(11, 0)]
+		[TestCase(12, 1)]
+		[TestCase(13, 1)]
+		[TestCase(18, 4)]
+		[TestCase(14, 2)]
+		[TestCase(16, 3)]
+		public void ModifiersAreCorrectForAttributeValues(int attributeValue, int expectedModifier)
+		{
+			Attributes testAttributes = new Attributes { Strength = attributeValue };
+			Assert.That (testAttributes.StrengthModifier, Is.EqualTo (expectedModifier));
+		}
 	}
 }
 
--- DnD5e_CBMobile_Core/Attributes.cs
+++ DnD5e_CBMobile_Core/Attributes.cs
@@ -5,10 +5,37 @@ namespace DnD5e_CBMobile_Core
 	public class Attributes
 	{
 		public int Strength { get; set; }
+		public int Dexterity { get; set; }
+		public int Constitution { get; set; }
+		public int Intelligence {get; set;}
+		public int Wisdom {get; set;}
+		public int Charisma {get; set;}
+
+		public int StrengthModifier { get{ return ConvertToModifier(Strength); }}
+		public int DexterityModifier { get{ return ConvertToModifier(Dexterity); }}
+		public int ConstitutionModifier{ get{ return ConvertToModifier(Constitution); }}
+		public int IntelligenceModifier { get{ return ConvertToModifier(Intelligence); }}
+		public int WisdomModifier { get{ return ConvertToModifier(Wisdom); }}
+		public int CharismaModifier { get{ return ConvertToModifier(Charisma); }}
+
+		private int ConvertToModifier (int attributeValue)
+		{
+			var baseLine = attributeValue - 10;
+			if (baseLine < 0) {
+				//because integer math is fun
+				return  (baseLine -1) / 2;
+			}
+			return baseLine/2;
+		}
 
 		public static Attributes operator +(Attributes op1, Attributes op2){
 			return new Attributes {
-				Strength = op1.Strength + op2.Strength
+				Strength = op1.Strength + op2.Strength,
+				Dexterity = op1.Dexterity + op2.Dexterity,
+				Constitution = op1.Constitution + op2.Constitution,
+				Intelligence = op1.Intelligence + op2.Intelligence,
+				Wisdom = op1.Wisdom + op2.Wisdom,
+				Charisma = op1.Charisma + op2.Charisma
 			};
 		}
 	}
