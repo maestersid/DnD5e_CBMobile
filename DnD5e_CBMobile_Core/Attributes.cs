90e9b8317ab20bfcc775596cceb9dc55edcd3f23
Neil Sorensen <nsorens@gmail.com>
2015-01-04 21:15:38 -0700

    Setting up a test project
    Created an attributes class and a basic test

---
+++ DnD5e_CBMobile_Core/Attributes.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace DnD5e_CBMobile_Core
+{
+	public class Attributes
+	{
+		public int Strength { get; set; }
+
+		public static Attributes operator +(Attributes op1, Attributes op2){
+			return new Attributes {
+				Strength = op1.Strength + op2.Strength
+			};
+		}
+	}
+}
+
